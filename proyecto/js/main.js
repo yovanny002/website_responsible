bf70b868d842d5f0f2795027e10f2e7719f64d51
yovanny002 <32279204+yovanny002@users.noreply.github.com>
2020-12-04 23:27:22 -0400

    Add files via upload

---
+++ proyecto/js/main.js
@@ -0,0 +1,6 @@
+function cambio(){
+    var secuencia = document.getElementById('id_banner');
+    var navegacion = document.getElementById('navegacion');
+    secuencia.classList.toggle('active');
+    navegacion.classList.toggle('active');
+}
